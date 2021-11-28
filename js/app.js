998e106a14fc6ba5fad171d18b68beb85bf4957a
KhantThuHtet <khantthu7007@gmail.com>
2021-11-28 21:00:12 +0630

    done sortable

--- js/app.js
+++ js/app.js
@@ -12,14 +12,16 @@ const richestPeople = [
 ];
 
 const draggableList = document.getElementById("draggable-list");
-const check = document.getElementById('check');
-
+const checkBtn = document.getElementById('check');
 function createList() {
-    richestPeople.map(el=>{
-        draggableList.innerHTML += `
-      <li>
-        <span class="number"></span>
-        <div class="draggable" draggable="true">
+    let shuffleArr = richestPeople.map((el) => ({el, sort: Math.random()}))
+                                .sort((x, y) => x.sort - y.sort)
+                                .map(val => val.el);
+    shuffleArr.map((el, index) => {
+      draggableList.innerHTML += `
+      <li class='dragArea'>
+        <span class="number">${index + 1}</span>
+        <div class="draggable" data-drag_index='${index}' draggable="true">
             <p class="person-name">
                 ${el}
             </p>
@@ -31,4 +33,80 @@ function createList() {
 createList();
 
 
+document.addEventListener("dragstart", (event) => {
+  const draggedID = event.target.dataset.drag_index;
+  event.dataTransfer.setData('draggedID', draggedID);
+  event.target.style.opacity = '0.5';
+});
+
+document.addEventListener('dragend', (event) =>{
+  event.target.style.opacity = '';
+});
+
+document.addEventListener('dragenter', (event) =>{
+  event.target.style.backgroundColor = '#eee';
+});
+
+document.addEventListener('dragleave', (event) =>{
+  event.target.style.backgroundColor = "#fff";
+});
+
+document.addEventListener('dragover', (event) =>{
+  event.preventDefault();
+});
+
+document.addEventListener('drop', (event)=>{
+  event.target.style.backgroundColor = "#fff";
+  
+  let dragStartID;
+  let dragEndID; 
+  let dragStart;
+  let dragEnd;
+  let dragStartValue;
+  let dragEndValue;
+
+  if (!event.target.classList.contains("draggable")) {
+    dragStartID = event.dataTransfer.getData("draggedID");
+    dragEndID = event.target.parentNode.dataset.drag_index;
+    dragStart = document.querySelector(`[data-drag_index='${dragStartID}']`);
+    dragEnd = document.querySelector(`[data-drag_index='${dragEndID}']`);
+    dragStartValue = dragStart.innerHTML;
+    dragEndValue = dragEnd.innerHTML;
+    dragStart.innerHTML = dragEndValue;
+    dragEnd.innerHTML = dragStartValue;
+    return;
+  }
+  dragStartID = event.dataTransfer.getData("draggedID");
+  dragEndID = event.target.dataset.drag_index;
+  dragStart = document.querySelector(`[data-drag_index='${dragStartID}']`);
+  dragEnd = document.querySelector(`[data-drag_index='${dragEndID}']`);
+  dragStartValue = dragStart.innerHTML;
+  dragEndValue = dragEnd.innerHTML;
+  dragStart.innerHTML = dragEndValue;
+  dragEnd.innerHTML = dragStartValue;
+  console.log(dragStartID, dragEndID);
+});
+
+checkBtn.addEventListener('click', ()=>{
+  let p = document.querySelectorAll('.person-name');
+  
+  let nameNodeList = Array.from(p);
+  let nameArr = nameNodeList.map(el=>{
+    return (el.textContent.trim());
+  });
+
+  richestPeople.forEach((el, index) =>{
+    if (el == nameArr[index]) {
+      let trueValueTag = document.querySelector(`[data-drag_index='${index}']`);
+      let trueName = trueValueTag.querySelector('p');
+      trueName.classList.remove('wrong');
+      trueName.classList.add('success');
+    }else{
+      let wrongValueTag = document.querySelector(`[data-drag_index='${index}']`);
+      let wrongName = wrongValueTag.querySelector("p");
+      wrongName.classList.remove('success');
+      wrongName.classList.add("wrong");
+    }
+  })
+});
 
